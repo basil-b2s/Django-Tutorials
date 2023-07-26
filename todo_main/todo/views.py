aea1ee2dafe34c8965409c73aabc4f3f11e91883
Basil Saji <basilsaji222@gmail.com>
2023-07-26 23:16:30 +0530

    added mark as done and undone feature

--- todo_main/todo/views.py
+++ todo_main/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from .models import Task
 # Create your views here.
@@ -7,3 +7,15 @@ def addTask(request):
     task = request.POST['task']
     Task.objects.create(task = task)
     return redirect("home")
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
